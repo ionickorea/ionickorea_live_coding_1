1b3cac44c921ac833806ebcedb45947013f432bc
Seunghoon Ko <imskojs@gmail.com>
2015-11-30 09:04:03 +0900

    (init) default structure from ionic creator

---
+++ www/js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+  
+.controller('로그인Ctrl', function($scope) {
+
+})
+   
+.controller('글목록Ctrl', function($scope) {
+
+})
+   
+.controller('아이오닉크리에이터Ctrl', function($scope) {
+
+})
+   
+.controller('새글쓰기Ctrl', function($scope) {
+
+})
+ 
